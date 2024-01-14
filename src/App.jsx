94d7df0a2b7769de2f3621e0dbb0ecf6cb4de555
Guilherme <guilhermepontifice.gp@gmail.com>
2024-01-14 10:12:46 -0300

    add: small changes in deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Header from './pages/Header'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
     <div className='bg-purple min-h-screen'>
-      <Header />
+      <Router>
+        <Routes>
+          <Route path='/movie-database/' element={<Header />} />
+        </Routes>
+      </Router >
     </div>
   )
 }
