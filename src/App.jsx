bcf593a63d5d4a782e7c213b546de57c4c1198c1
Guilherme <guilhermepontifice.gp@gmail.com>
2024-01-15 09:46:22 -0300

    add: deploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <div className='bg-purple min-h-screen'>
-      <Router>
         <Routes>
           <Route path='/movie-database' element={<Header />} />
         </Routes>
-      </Router >
     </div>
   )
 }
