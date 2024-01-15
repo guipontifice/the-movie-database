784f70797cc9e43efcaf0365eeb3683fcdd49b0f
Guilherme <guilhermepontifice.gp@gmail.com>
2024-01-15 11:01:36 -0300

    add: deploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Header from './pages/Header'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
 function App() {
   return (
     <div className='bg-purple min-h-screen'>
         <Routes>
-          <Route path='/movie-database' element={<Header />} />
+          <Route path='/the-movie-database/' element={<Header />} />
         </Routes>
     </div>
   )
