b3390cc067c61c550b80dd32b639cdf8c8d9e881
Guilherme <guilhermepontifice.gp@gmail.com>
2024-01-15 08:22:17 -0300

    add: deployment fix

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-database',
+  base: '/movie-database/',
   build: {
     outDir: 'dist',
-    target: 'es2015',
+    manifest: true, 
+    input: 'src/index.html'
   }
 })
